e789a447c8dcd73a990ee63d644e8e983ab491db
Wsmallnews <1371606921@qq.com>
2024-12-26 22:11:22 +0800

    可创建多级分类

--- src/Resources/Pages/Category.php
+++ src/Resources/Pages/Category.php
@@ -6,49 +6,57 @@ use Filament\Actions\Action;
 use Filament\Forms\Components;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
-use Filament\Pages\Concerns\CanUseDatabaseTransactions;
-use Filament\Pages\Concerns\HasUnsavedDataChangesAlert;
-use Filament\Pages\Concerns\InteractsWithFormActions;
-use Filament\Pages\Page;
-use Filament\Support\Exceptions\Halt;
-use Illuminate\Database\Eloquent\Model;
 use Livewire\Attributes\Locked;
-use Throwable;
 use Wsmallnews\Category\Category as CategoryManager;
 use Wsmallnews\Category\Enums;
 use Wsmallnews\Category\Models\CategoryType;
-use Wsmallnews\Support\Concerns\RepeaterTree;
+use Wsmallnews\Support\Resources\Pages\FormPage;
+use Wsmallnews\Support\Traits\Resources\SetResource;
 
-class Category extends Page
+class Category extends FormPage
 {
-    use CanUseDatabaseTransactions;
-    use HasUnsavedDataChangesAlert;
-    use InteractsWithFormActions;
-    use RepeaterTree;
+    use SetResource;
 
     protected static ?string $navigationGroup = '分类管理';
 
     protected static ?string $navigationLabel = '分类管理';
 
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'iconsax-bol-category';
 
-    protected static string $view = 'sn-category::resources.pages.category';
+    protected static ?string $title = '分类管理';
+
+    protected static ?string $slug = 'category/{scope_type}/{scope_id?}';
 
     #[Locked]
-    public Model | int | string | null $record = null;
+    public string $scope_type = 'default';
 
-    /**
-     * @var array<string, mixed> | null
-     */
-    public ?array $data = [];
+    #[Locked]
+    public int $scope_id = 0;
 
-    // public function mount(int | string | null $record): void
-    public function mount(): void
+
+    public function mount()
     {
-        $this->record = CategoryType::where('scope_type', 'shop')->first();
+        $this->record = CategoryType::where('scope_type', $this->scope_type)->where('scope_id', $this->scope_id)->first();
+
+
+        if (!$this->record) {
+            // @sn todo 这么创建太随意了，要提前创建好
+            $categoryType = new CategoryType();
+            $categoryType->scope_type = $this->scope_type;
+            $categoryType->scope_id = $this->scope_id;
+            $categoryType->name = $this->scope_type;
+            $categoryType->level = 1;
+            $categoryType->status = Enums\CategoryTypeStatus::Normal;
+
+            $categoryType->save();
+
+            $this->record = $categoryType;
+        }
 
         $this->fillForm();
     }
+
+
 
     public function form(Form $form): Form
     {
@@ -62,8 +70,7 @@ class Category extends Page
                             ->required(),
                         Components\TextInput::make('description')
                             ->label('类型描述')
-                            ->placeholder('请输入分类类型描述')
-                            ->required(),
+                            ->placeholder('请输入分类类型描述'),
                         Components\Radio::make('level')
                             ->label('分类级别')
                             ->options([
@@ -89,9 +96,6 @@ class Category extends Page
                     ->schema(fn (Get $get): array => [
                         $this->repeaterField($this->getFieldsTree($get('level')), relation_name: 'categories')->hiddenLabel(),
                     ])
-                    // ->schema([
-                    //     $this->repeaterField($this->getFieldsTree(3), relation_name: 'categories')->hiddenLabel(),
-                    // ])
                     ->columns(1),
             ]);
     }
@@ -141,12 +145,11 @@ class Category extends Page
                         ->uploadingMessage('分类图片上传中...')
                         ->columnSpan(1),
                 ])
-                    ->columns(2),
+                ->columns(2),
 
                 Components\TextInput::make('description')
                     ->hiddenLabel()
-                    ->placeholder('请输入分类描述')
-                    ->required(),
+                    ->placeholder('请输入分类描述'),
 
                 Components\Radio::make('status')
                     ->hiddenLabel()
@@ -155,160 +158,16 @@ class Category extends Page
                     ->inline()
                     ->required(),
             ])
-                ->columns(4)
-                ->columnSpanFull(),
+            ->columns(4)
+            ->columnSpanFull(),
         ];
     }
 
-    public function getRecord(): ?CategoryType
+
+    public static function getNavigationUrl(): string
     {
-        return $this->record;
-    }
-
-    protected function fillForm(): void
-    {
-        $this->callHook('beforeFill');
-
-        $data = $this->mutateFormDataBeforeFill($this->getRecord()?->attributesToArray() ?? []);
-
-        $this->form->fill($data);
-
-        $this->callHook('afterFill');
-    }
-
-    /**
-     * @param  array<string, mixed>  $data
-     * @return array<string, mixed>
-     */
-    protected function mutateFormDataBeforeFill(array $data): array
-    {
-        return $data;
-    }
-
-    public function save(): void
-    {
-        try {
-            $this->beginDatabaseTransaction();
-
-            $this->callHook('beforeValidate');
-
-            $data = $this->form->getState();
-
-            $this->callHook('afterValidate');
-
-            $data = $this->mutateFormDataBeforeSave($data);
-
-            $this->callHook('beforeSave');
-
-            // $settings = app(static::getSettings());
-
-            // $settings->fill($data);
-            // $settings->save();
-
-            // 这里保存分类呀
-
-            $this->callHook('afterSave');
-
-            $this->commitDatabaseTransaction();
-        } catch (Halt $exception) {
-            $exception->shouldRollbackDatabaseTransaction() ?
-                $this->rollBackDatabaseTransaction() :
-                $this->commitDatabaseTransaction();
-
-            return;
-        } catch (Throwable $exception) {
-            $this->rollBackDatabaseTransaction();
-
-            throw $exception;
-        }
-
-        $this->rememberData();
-
-        $this->getSavedNotification()?->send();
-
-        if ($redirectUrl = $this->getRedirectUrl()) {
-            $this->redirect($redirectUrl, navigate: FilamentView::hasSpaMode() && is_app_url($redirectUrl));
-        }
-    }
-
-    public function getSavedNotification(): ?Notification
-    {
-        $title = $this->getSavedNotificationTitle();
-
-        if (blank($title)) {
-            return null;
-        }
-
-        return Notification::make()
-            ->success()
-            ->title($title);
-    }
-
-    public function getSavedNotificationTitle(): ?string
-    {
-        return $this->getSavedNotificationMessage() ?? __('filament-spatie-laravel-settings-plugin::pages/settings-page.notifications.saved.title');
-    }
-
-    /**
-     * @deprecated Use `getSavedNotificationTitle()` instead.
-     */
-    protected function getSavedNotificationMessage(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * @param  array<string, mixed>  $data
-     * @return array<string, mixed>
-     */
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        return $data;
-    }
-
-    /**
-     * @return array<Action | ActionGroup>
-     */
-    public function getFormActions(): array
-    {
-        return [
-            $this->getSaveFormAction(),
-        ];
-    }
-
-    public function getSaveFormAction(): Action
-    {
-        return Action::make('save')
-            // ->label(__('filament-spatie-laravel-settings-plugin::pages/settings-page.form.actions.save.label'))
-            ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
-            ->submit('save')
-            ->keyBindings(['mod+s']);
-    }
-
-    public function getSubmitFormAction(): Action
-    {
-        return $this->getSaveFormAction();
-    }
-
-    /**
-     * @return array<int | string, string | Form>
-     */
-    protected function getForms(): array
-    {
-        return [
-            'form' => $this->form(
-                $this->makeForm()
-                    ->schema($this->getFormSchema())
-                    ->model($this->getRecord() ?? new CategoryType)
-                    ->statePath('data')
-                    ->columns(2)
-                    ->inlineLabel($this->hasInlineLabels()),
-            ),
-        ];
-    }
-
-    public function getRedirectUrl(): ?string
-    {
-        return null;
+        return static::getUrl(parameters: [
+            'scope_type' => 'smallnews',
+        ]);
     }
 }
