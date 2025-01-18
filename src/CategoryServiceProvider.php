3cefe1a3540c57ed974236bfe08b7ccc4c943a63
smallnews@qq.com <1371606921@qq.com>
2025-01-18 18:37:57 +0800

    runMigrations

--- src/CategoryServiceProvider.php
+++ src/CategoryServiceProvider.php
@@ -50,6 +50,7 @@ class CategoryServiceProvider extends PackageServiceProvider
 
         if (file_exists($package->basePath('/../database/migrations'))) {
             $package->hasMigrations($this->getMigrations());
+            $package->runsMigrations();
         }
 
         // if (file_exists($package->basePath('/../resources/lang'))) {
