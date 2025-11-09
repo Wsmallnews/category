2adb964af4a28bebd538be9846ae37f08cda7a13
smallnews <1371606921@qq.com>
2025-11-09 21:30:18 +0800

    把先获取 CategoryType 封装 traits

---
+++ src/Livewire/Concerns/Categoryable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wsmallnews\Category\Livewire\Concerns;
+
+use Kalnoy\Nestedset\QueryBuilder;
+use Livewire\Attributes\Locked;
+use Wsmallnews\Category\Models\Category as CategoryModel;
+use Wsmallnews\Category\Models\CategoryType as CategoryTypeModel;
+
+trait Categoryable
+{
+    #[Locked]
+    public ?int $categoryTypeId = null;
+
+    public ?CategoryTypeModel $categoryType = null;
+
+    public function mountCategoryable()
+    {
+        $this->categoryType = CategoryTypeModel::scopeable(...$this->getScopeable())->when($this->categoryTypeId, function ($query) {
+            $query->where('id', $this->categoryTypeId);
+        })->firstOrFail();
+
+        $this->categoryTypeId = $this->categoryType->id;
+    }
+
+    
+    public function getScoped()
+    {
+        $scoped = [
+            ...$this->getScopeable(),
+            'type_id' => $this->categoryType->id,
+        ];
+        has_tenancy() && $scoped['team_id'] = current_tenant()?->id;
+
+        return $scoped;
+    }
+
+
+    /**
+     * queryBuilder 不支持调用 Nestedset 的 scoped 方法
+     */
+    public function getScopedQuery(): string | QueryBuilder
+    {
+        return CategoryModel::scoped($this->getScoped());
+    }
+}
