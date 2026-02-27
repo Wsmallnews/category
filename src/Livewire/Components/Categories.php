d7dcb05e1b5da31964999609b9b03af7d3f86b10
smallnews@qq.com <1371606921@qq.com>
2026-02-27 10:51:06 +0800

    分类支持 url 跳转当前分类

--- src/Livewire/Components/Categories.php
+++ src/Livewire/Components/Categories.php
@@ -5,19 +5,41 @@ namespace Wsmallnews\Category\Livewire\Components;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Wsmallnews\Category\Livewire\Concerns\Categoryable;
 use Wsmallnews\FilamentNestedset\Livewire\Components\Nestedset;
 use Wsmallnews\Support\Livewire\Concerns\Scopeable;
+
+use function \Filament\Support\generate_href_html;
 
 class Categories extends Nestedset
 {
     use Categoryable;
     use Scopeable;
 
+    public bool $useUrl = false;
+
+    public ?string $url = null;
+
+    public string $queryName = 'categoryId';
+
+    public bool $shouldOpenInNewTab = false;
+
     public function getRecordLabel(Model $record): HtmlString | string
     {
         return $record->name_label;
+    }
+
+    public function getRecordUrl(Model $record): string | HtmlString | null
+    {
+        // 启用 url 方式，并且没有子导航时，才返回 url
+        if ($this->useUrl && ! $record->children->count()) {
+            $url = $this->url ?? request()->fullUrlWithoutQuery($this->queryName);      // 默认使用当前 url, 移除 queryName 参数， 重新拼接新的 queryName 参数
+            return generate_href_html($url . (Str::contains($url, '?') ? '&' : '?') . $this->queryName . '=' . $record->id, $this->shouldOpenInNewTab);
+        }
+
+        return null;
     }
 
     public function getHasActive(Model $record): bool
