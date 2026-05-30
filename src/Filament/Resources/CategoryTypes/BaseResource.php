989eafaa76d981346c3722dbb257f03226728ddb
Wsmallnews <16216215+Wsmallnews@users.noreply.github.com>
2026-05-30 08:40:11 +0000

    Fix styling

--- src/Filament/Resources/CategoryTypes/BaseResource.php
+++ src/Filament/Resources/CategoryTypes/BaseResource.php
@@ -20,9 +20,9 @@ abstract class BaseResource extends Resource
 {
     use Scopeable;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::Bars3;
+    protected static string | BackedEnum | null $navigationIcon = Heroicon::Bars3;
 
-    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::Bars3;
+    protected static string | BackedEnum | null $activeNavigationIcon = Heroicon::Bars3;
 
     protected static ?string $slug = 'category-types';
 
@@ -50,7 +50,7 @@ abstract class BaseResource extends Resource
         return static::$navigationLabel ?? __('sn-category::category.category_type_resource.navigation_label');
     }
 
-    public static function getNavigationGroup(): string|UnitEnum|null
+    public static function getNavigationGroup(): string | UnitEnum | null
     {
         return static::$navigationGroup ?? __('sn-category::category.global_default.navigation_group');
     }
