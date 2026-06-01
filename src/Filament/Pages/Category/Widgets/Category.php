4a3083e09a6a1c118426e0e33aa734848306418c
Wsmallnews <16216215+Wsmallnews@users.noreply.github.com>
2026-06-01 14:23:36 +0000

    Fix styling

--- src/Filament/Pages/Category/Widgets/Category.php
+++ src/Filament/Pages/Category/Widgets/Category.php
@@ -38,7 +38,7 @@ class Category extends Nestedset
         return static::$modelLabel ?? __('sn-category::category.category_page.model_label');
     }
 
-    public static function getRecordLabel(Model $record): HtmlString|string
+    public static function getRecordLabel(Model $record): HtmlString | string
     {
         return $record->name_label;
     }
