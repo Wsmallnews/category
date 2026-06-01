1d7f6a17bb9e9746aca0b16bfb75f9435af5afdd
smallnews <1371606921@qq.com>
2026-06-01 22:23:04 +0800

    修复小部件的 nestedScoped 方法

--- src/Filament/Pages/Category/Widgets/Category.php
+++ src/Filament/Pages/Category/Widgets/Category.php
@@ -2,12 +2,9 @@
 
 namespace Wsmallnews\Category\Filament\Pages\Category\Widgets;
 
-use Filament\Facades\Filament;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\HtmlString;
-use Illuminate\Support\Str;
 use Livewire\Attributes\Reactive;
-use Wsmallnews\Category\Enums\CategoryTypeStatus;
 use Wsmallnews\Category\Filament\Pages\Category\Schemas\CategoryForm;
 use Wsmallnews\Category\Filament\Pages\Category\Schemas\CategoryInfolist;
 use Wsmallnews\Category\Models\CategoryType;
@@ -22,6 +19,15 @@ class Category extends Nestedset
     #[Reactive]
     public ?CategoryType $record = null;
 
+    protected static ?CategoryType $categoryType = null;
+
+    public function mount(): void
+    {
+        parent::mount();
+
+        static::$categoryType = $this->record;
+    }
+
     public static function getModel(): ?string
     {
         return Utils::getCategoryModel();
@@ -32,19 +38,17 @@ class Category extends Nestedset
         return static::$modelLabel ?? __('sn-category::category.category_page.model_label');
     }
 
-    public static function getRecordLabel(Model $record): HtmlString | string
+    public static function getRecordLabel(Model $record): HtmlString|string
     {
         return $record->name_label;
     }
 
     public static function nestedScoped(): array
     {
-        $categoryType = static::getCategoryType();
-
         return [
-            'scope_type' => $categoryType?->scope_type,
-            'scope_id' => $categoryType?->scope_id,
-            'type_id' => $categoryType?->id,
+            'scope_type' => static::$categoryType?->scope_type,
+            'scope_id' => static::$categoryType?->scope_id,
+            'type_id' => static::$categoryType?->id,
         ];
     }
 
@@ -56,25 +60,5 @@ class Category extends Nestedset
     public static function infolistSchema(): array
     {
         return CategoryInfolist::infolist();
-    }
-
-    public static function getCategoryType(): ?CategoryType
-    {
-        $categoryType = Utils::getCategoryTypeModel()::query()
-            ->snScope(static::getScopeType(), static::getScopeId())
-            ->first();
-
-        if (! $categoryType && ! static::getCanManage()) {
-            // 自动创建分类类型
-            $categoryType = Utils::getCategoryTypeModel()::create([
-                'name' => Str::title(static::getScopeType()),
-                'level' => static::getLevel(),
-                'status' => CategoryTypeStatus::Normal,
-                ...static::getScopeable(),
-                'team_id' => Filament::getTenant()?->id,
-            ]);
-        }
-
-        return $categoryType;
     }
 }
